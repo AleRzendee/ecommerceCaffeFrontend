1139bb18140c598d7c1492e273b6d22c6c038775
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-04-04 15:55:44 -0300

    Routes Config v1; #0002 04/04/25

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home.component';
+import { ProductListComponent } from './pages/product-list.component';
+import { ProductDetailsComponent } from './pages/product-details.component';
+import { CartComponent } from './pages/cart.component';
+import { CheckoutComponent } from './pages/checkout.component';
+import { AuthComponent } from './pages/auth.component';
+import { NotFoundComponent } from './shared/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'produtos', component: ProductListComponent },
+  { path: 'produto/:id', component: ProductDetailsComponent },
+  { path: 'carrinho', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: '**', component: NotFoundComponent }, // Rota coringa para páginas não encontradas
+];
