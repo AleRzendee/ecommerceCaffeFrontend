c2d8c16689b3231f15db4d3ad3199a32be82e328
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-04-05 15:34:23 -0300

    Footer Config; #0003

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/navbar/navbar.component';
+import { FooterComponent } from './shared/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
--- src/app/shared/footer.component.ts
+++ src/app/shared/footer.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.sass'
 })
 export class FooterComponent {
-
+  currentYear = new Date().getFullYear();
 }
+
